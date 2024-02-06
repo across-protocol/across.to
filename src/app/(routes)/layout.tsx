1ca8ff735d6207268445ce49a66cd96f9cea5ba9
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-02-06 15:42:25 +0100

    feat: custom tw config based on figma (#1)
    * feat: custom tw config based on figma
    
    * feat: add tailwind-merge

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+import { Barlow } from "next/font/google";
 
 import HeaderNav from "@/app/_components/header-nav";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Barlow({ subsets: ["latin"], weight: ["200", "300", "400", "500"] });
 
 export const metadata: Metadata = {
   title: {
@@ -21,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={twMerge(inter.className, "bg-grey-dark text-light-300")}>
         <HeaderNav />
         {children}
       </body>
