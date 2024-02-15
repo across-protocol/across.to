16e734a8c8ae5a11e27f0b4d8099c6260c3943f7
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-02-15 08:41:06 +0100

    feat(plus): chains + get started sections

---
+++ src/app/(routes)/across-plus/_components/get-started-section.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+import { ArrowRightIcon } from "@/app/_components/icons";
+import { Text } from "@/app/_components/text";
+import plusGetStartedImage from "@/app/_assets/plus-get-started.png";
+import { INFORMATION_LINKS } from "@/app/_constants";
+
+export function GetStartedSection() {
+  return (
+    <section className="container mx-auto flex flex-col items-center gap-16 sm:gap-24">
+      <Image src={plusGetStartedImage} alt="get started image" />
+      <Text variant="heading-1">Ready to get started?</Text>
+      <div className="flex flex-row gap-6">
+        <a
+          href={INFORMATION_LINKS.docs.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex flex-col items-center justify-center rounded-full bg-teal-100/[.05] px-6 py-2 shadow-sm"
+        >
+          <Text variant="cap-case" className="text-teal-100 sm:text-sm md:text-sm">
+            go to docs
+          </Text>
+        </a>
+        {/* TODO: Use correct link */}
+        <a href="#" className="flex flex-row items-center justify-center gap-2 px-6 py-2">
+          <Text variant="cap-case" className="text-teal-100 sm:text-sm md:text-sm">
+            get it touch
+          </Text>
+          <div className="flex h-5 w-5 flex-col items-center justify-center rounded-full bg-teal-100">
+            <ArrowRightIcon />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/(routes)/across-plus/_components/supported-chains-section.tsx
@@ -0,0 +1,63 @@
+import {
+  EthereumIcon,
+  OptimismIcon,
+  PolygonIcon,
+  ArbitrumIcon,
+  BaseIcon,
+  ZkSyncIcon,
+} from "@/app/_components/icons";
+import { Text } from "@/app/_components/text";
+
+const chains = [
+  {
+    label: "Ethereum Mainnet",
+    Icon: EthereumIcon,
+  },
+  {
+    label: "Optimism",
+    Icon: OptimismIcon,
+  },
+  {
+    label: "Polygon Network",
+    Icon: PolygonIcon,
+  },
+  {
+    label: "Arbitrum One",
+    Icon: ArbitrumIcon,
+  },
+  {
+    label: "Base",
+    Icon: BaseIcon,
+  },
+  {
+    label: "zkSync Era",
+    Icon: ZkSyncIcon,
+  },
+];
+
+export function SupportedChainsSection() {
+  return (
+    <section className="container mx-auto flex flex-col gap-16 p-5 sm:gap-24 md:px-4">
+      <div className="flex flex-col gap-4">
+        <Text variant="cap-case" className="text-center text-teal-100">
+          well connected
+        </Text>
+        <Text variant="heading-2" className="text-center capitalize text-light-200">
+          Supported Chains
+        </Text>
+      </div>
+      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3">
+        {chains.map((chain, index) => (
+          <div key={index} className="flex flex-col items-center gap-4">
+            <div className="flex h-20 w-20 flex-col items-center justify-center rounded-2xl bg-teal-100/[.05]">
+              <chain.Icon className="h-10 w-10" variant="teal" />
+            </div>
+            <Text variant="body" className="capitalize text-light-300">
+              {chain.label}
+            </Text>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
