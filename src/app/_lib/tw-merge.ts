5ad0a5eaf57a636c8ece4f913b98514c47ed88c3
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-02-20 09:54:10 +0100

    fixup

--- src/app/_lib/tw-merge.ts
+++ src/app/_lib/tw-merge.ts
@@ -39,7 +39,7 @@ export const twMerge = extendTailwindMerge({
       "max-w": ["max-w-100"],
       animate: [
         {
-          animate: ["scroll-infinite"],
+          animate: ["infinite-scroll"],
         },
       ],
       "drop-shadow": [
