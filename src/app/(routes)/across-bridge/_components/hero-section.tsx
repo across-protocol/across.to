88754a1e77a69de65ceaa1eb5154ade812f15fe6
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-02-26 10:11:14 +0100

    fix: remove unnecessary min-h-screen from hero (#34)

--- src/app/(routes)/across-bridge/_components/hero-section.tsx
+++ src/app/(routes)/across-bridge/_components/hero-section.tsx
@@ -9,7 +9,7 @@ import bridgeHeroImage from "@/app/_assets/bridge-hero.png";
 
 export function HeroSection() {
   return (
-    <Hero className="md:-mb-[180px]">
+    <Hero>
       <div className="container mx-auto flex flex-col items-center gap-16 px-4 pb-16 pt-8 md:flex-row-reverse md:gap-8 md:pt-16">
         <div className="flex max-w-80 flex-1 sm:max-w-100 md:max-w-full">
           <Image src={bridgeHeroImage} alt="Across bridge ui" priority={true} />
