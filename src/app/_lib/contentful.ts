c679fb92c446bc0425e6456d72997fee0897b203
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2024-09-17 11:58:44 -0400

    improve: use sort by publish date (#66)
    * improve: use sort by publish date
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * fix: recent articles in homepage
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    ---------
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- src/app/_lib/contentful.ts
+++ src/app/_lib/contentful.ts
@@ -50,11 +50,13 @@ export async function retrieveContentfulPublishedSlugs({
   limit,
   avoidTags,
   includeTags,
+  sortByRecent,
 }: {
   query?: string;
   limit?: number;
   avoidTags?: string[];
   includeTags?: string[];
+  sortByRecent?: boolean;
 } = {}): Promise<string[]> {
   const client = getProductionClient();
   const options = {
@@ -66,6 +68,7 @@ export async function retrieveContentfulPublishedSlugs({
     ...(query ? { query } : {}),
     ...(avoidTags ? { "fields.tag[nin]": avoidTags.join(",").toLowerCase() } : {}),
     ...(includeTags ? { "fields.tag[in]": includeTags.join(",").toLowerCase() } : {}),
+    ...(sortByRecent ? { order: "-fields.publishDate" } : {}),
   } as const;
   const entries =
     await client.withoutUnresolvableLinks.getEntries<TypeAcrossBlogPostSkeleton>(options);
@@ -113,10 +116,8 @@ export async function retrieveRelevantContentfulEntries(
     "fields.tag[in]": tags.join(",").toLowerCase(), // get posts with same tags
     "fields.slug[nin]": entrySlugId, // don't include current post
     "fields.slug[exists]": true, // no empty slugs
-
-    // TODO: Update existing posts to have publishDate
-    // "fields.publishDate[exists]": true, // no empty dates
-    // order: "-fields.publishDate", // sorted latest first
+    "fields.publishDate[exists]": true, // no empty dates
+    order: "-fields.publishDate", // sorted latest first
   } as const;
   const entries =
     await client.withoutUnresolvableLinks.getEntries<TypeAcrossBlogPostSkeleton>(options);
