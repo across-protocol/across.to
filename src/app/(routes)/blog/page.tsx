c9c3af13222ee3a0aa2fdc08a196e1a36752076c
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-09-17 16:36:27 -0400

    improve: remove getting started
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- src/app/(routes)/blog/page.tsx
+++ src/app/(routes)/blog/page.tsx
@@ -4,10 +4,7 @@ import BackgroundBanner from "./_components/background-banner";
 import Filter from "./_components/filter";
 
 import { Suspense } from "react";
-import {
-  retrieveContentfulPublishedSlugs,
-  retrieveContentfulEntry,
-} from "@/app/_lib/contentful";
+import { retrieveContentfulPublishedSlugs } from "@/app/_lib/contentful";
 import { Posts } from "./_components/posts";
 import { createCacheKey } from "@/app/_lib/cache";
 
@@ -18,28 +15,14 @@ type PageProps = {
 };
 
 export default async function BlogHomePage({ searchParams }: PageProps) {
-  const recentArticleSlugs = await retrieveContentfulPublishedSlugs({
-    limit: 6,
-    avoidTags: ["get-started"],
-    sortByRecent: true,
-  });
-  const getStartedSlugs = await retrieveContentfulPublishedSlugs({
-    limit: 3,
-    includeTags: ["get-started"],
-  });
-  const getStartedSnippets = await Promise.all(
-    getStartedSlugs.map((s) => retrieveContentfulEntry(s)),
-  );
-
   const key = createCacheKey({
     searchParams,
   });
-
   return (
     <>
       <BackgroundBanner />
       <main className="relative z-[1] mx-auto flex w-full max-w-4xl flex-col items-center gap-6 px-4 py-10 lg:gap-8 lg:px-0">
-        <Text variant="heading-2" className="-mb-6 lg:-mb-8">
+        <Text variant="heading-2" className="-mb-6">
           Across Blog
         </Text>
         <Suspense>
@@ -51,11 +34,7 @@ export default async function BlogHomePage({ searchParams }: PageProps) {
             <h2 className="text-text-secondary text-2xl my-auto flex-1">Searching...</h2>
           }
         >
-          <Posts
-            getStartedSnippets={getStartedSnippets}
-            recentArticleSlugs={recentArticleSlugs}
-            searchParams={searchParams}
-          />
+          <Posts searchParams={searchParams} />
           <BackToTopButton />
         </Suspense>
       </main>
