8efb8590fc4c48648b7e1cfd48435bcf9bf0fd34
Gerhard Steenkamp <gerhard@umaproject.org>
2026-01-29 15:26:32 +0200

    add megaeth icons
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

---
+++ src/app/_components/icons/gradient/logos/megaeth.tsx
@@ -0,0 +1,112 @@
+import { LogoIconProps } from "./types";
+
+export function MegaEthIcon({ variant = "aqua", ...props }: LogoIconProps) {
+  const linearGradientBasedId = `paint_linear_gradient_${variant}`;
+  return (
+    <svg
+      width="177"
+      height="177"
+      viewBox="0 0 177 170"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M105.784 132.166C111.22 132.166 115.61 127.953 115.61 122.755C115.61 117.558 111.22 113.344 105.784 113.344C100.39 113.344 96 117.558 96 122.755C96 127.953 100.39 132.166 105.784 132.166Z"
+        fill={`url(#${linearGradientBasedId}_1)`}
+      />
+      <path
+        d="M70.572 132.446C76.0077 132.446 80.3567 128.233 80.3567 123.035C80.3567 117.838 76.0077 113.624 70.572 113.624C65.178 113.624 60.7876 117.838 60.7876 123.035C60.7876 128.233 65.178 132.446 70.572 132.446Z"
+        fill={`url(#${linearGradientBasedId}_1)`}
+      />
+      <path
+        d="M49.1176 34.1281H72.4612C76.8538 45.5415 88.3163 77.8796 89.1529 79.6814C89.3621 78.7806 100.908 43.5393 104.171 34.3283H128.435V114.723C125.423 113.121 122.411 111.519 119.148 109.717C116.888 108.616 114.797 107.414 112.496 106.513C112.287 90.8948 112.078 75.3765 111.576 59.1576C108.313 68.4685 97.0593 101.307 96.1389 102.208H81.1207C81.1207 102.208 66.5204 62.3612 65.8092 60.5592C65.6001 75.8771 65.3909 91.1951 64.8471 107.014C55.8946 111.419 50.5819 113.922 48.9087 114.522V34.1281H49.1176Z"
+        fill={`url(#${linearGradientBasedId}_1)`}
+      />
+      <path
+        d="M88.5209 13.5159C129.477 13.5159 162.944 45.5535 162.944 84.9998C162.944 124.446 129.602 156.484 88.5209 156.484C47.4399 156.484 14.098 124.446 14.098 84.9998C14.098 45.5535 47.4399 13.5159 88.5209 13.5159ZM88.5209 0C39.617 0 0 38.0449 0 84.9998C0 131.955 39.617 170 88.5209 170C137.383 170 177 131.955 177 84.9998C177 38.0449 137.383 0 88.5209 0Z"
+        fill={`url(#${linearGradientBasedId}_1)`}
+      />
+      <path
+        d="M88.5209 13.5159C129.477 13.5159 162.944 45.5535 162.944 84.9998C162.944 124.446 129.602 156.484 88.5209 156.484C47.4399 156.484 14.098 124.446 14.098 84.9998C14.098 45.5535 47.4399 13.5159 88.5209 13.5159ZM88.5209 0C39.617 0 0 38.0449 0 84.9998C0 131.955 39.617 170 88.5209 170C137.383 170 177 131.955 177 84.9998C177 38.0449 137.383 0 88.5209 0Z"
+        fill={`url(#${linearGradientBasedId}_1)`}
+      />
+      <defs>
+        <filter
+          id="filter0_b_564_7528"
+          x="-13.5"
+          y="-13"
+          width="72"
+          height="73"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feGaussianBlur in="BackgroundImageFix" stdDeviation="6.75" />
+          <feComposite
+            in2="SourceAlpha"
+            operator="in"
+            result="effect1_backgroundBlur_564_7528"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_backgroundBlur_564_7528"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="paint_linear_gradient_aqua_1"
+          x1="22.5649"
+          y1="0.641113"
+          x2="22.5649"
+          y2="45.6088"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.234414" stopColor="#6CF9D8" />
+          <stop offset="0.386534" stopColor="#B1FFED" />
+          <stop offset="0.508728" stopColor="white" />
+          <stop offset="0.513716" stopColor="#009A76" />
+          <stop offset="0.620948" stopColor="#C1FFF0" />
+          <stop offset="0.708229" stopColor="#29997F" />
+          <stop offset="0.78803" stopColor="#ADFFEC" />
+        </linearGradient>
+        <linearGradient
+          id="paint_linear_gradient_purple_1"
+          x1="22.5649"
+          y1="0.641113"
+          x2="22.5649"
+          y2="72.5005"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.234414" stopColor="#A26CF9" />
+          <stop offset="0.386534" stopColor="#CEB1FF" />
+          <stop offset="0.508728" stopColor="white" />
+          <stop offset="0.513716" stopColor="#3B009A" />
+          <stop offset="0.620948" stopColor="#D8C1FF" />
+          <stop offset="0.708229" stopColor="#542999" />
+          <stop offset="0.78803" stopColor="#CCADFF" />
+        </linearGradient>
+        <linearGradient
+          id="paint_linear_gradient_teal_1"
+          x1="22.5649"
+          y1="0.641113"
+          x2="22.5649"
+          y2="45.6088"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.234414" stopColor="#6CD3F9" />
+          <stop offset="0.386534" stopColor="#B1EAFF" />
+          <stop offset="0.508728" stopColor="white" />
+          <stop offset="0.513716" stopColor="#00719A" />
+          <stop offset="0.620948" stopColor="#C1EEFF" />
+          <stop offset="0.708229" stopColor="#297B99" />
+          <stop offset="0.78803" stopColor="#ADE9FF" />
+        </linearGradient>
+        <clipPath id="clip0_2_7">
+          <rect width="45" height="45" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
