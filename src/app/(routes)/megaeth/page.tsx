040ec606b89f7b011783a26c39aed2d53c13765e
Kanishk Khurana <kanishkkhurana152@gmail.com>
2026-02-10 01:13:09 +0530

    chore: remove tokens

--- src/app/(routes)/megaeth/page.tsx
+++ src/app/(routes)/megaeth/page.tsx
@@ -5,14 +5,14 @@ import { EcosystemSection } from './_components/ecosystem-section'
 import { FeaturesSection } from './_components/features-section'
 import { HeroSection } from './_components/hero-section'
 import { CreativeCta } from './_components/creative-cta'
-import { TokensSection } from './_components/tokens-section'
+// import { TokensSection } from './_components/tokens-section'
 
 export default function MegaEthLanding() {
   return (
     <div className="relative isolate overflow-hidden bg-[#151518] text-light-200">
       <HeroSection />
       <FeaturesSection />
-      <TokensSection />
+      {/* <TokensSection /> */}
       <BridgeGuideSection />
       <EcosystemSection />
       <CreativeCta />
