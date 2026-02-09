c203d25618d802918997204267753729a2633832
Kanishk Khurana <kanishkkhurana152@gmail.com>
2026-02-10 03:28:24 +0530

    fix: section headings in megaeth

--- src/app/(routes)/megaeth/_components/section-heading.tsx
+++ src/app/(routes)/megaeth/_components/section-heading.tsx
@@ -5,7 +5,7 @@ export function SectionHeading({ title }: { title: string }) {
     <div className="mx-auto max-w-4xl py-12 text-center">
       <Text
         variant="heading-3"
-        className="inline-block bg-clip-text text-heading-1 font-normal leading-tight  text-[#E0F3FF] text-transparent sm:text-heading-2"
+        className="inline-block bg-clip-text text-heading-1 font-normal leading-tight  text-[#E0F3FF] sm:text-heading-2"
       >
         {title}
       </Text>
