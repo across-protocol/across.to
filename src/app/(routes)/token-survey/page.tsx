71f9b4cd717fda554c09ff778f936b5ce6911ba6
Kanishk Khurana <kanishkkhurana152@gmail.com>
2026-03-07 13:34:52 +0530

    chore: language

--- src/app/(routes)/token-survey/page.tsx
+++ src/app/(routes)/token-survey/page.tsx
@@ -13,7 +13,7 @@ export default function TokenSurveyPage() {
     <main className="relative z-[1] mx-auto flex w-full max-w-5xl flex-col gap-8 px-4 py-10 lg:px-0">
       <div className="flex flex-col gap-4">
         <Text variant="heading-2" className="leading-[52.80px] ">
-        Bridge Across - Exchange Indication Survey
+        The Bridge Across - Exchange Indication Survey
         </Text>
         <Text variant="body" className=" text-grey-400 leading-relaxed">
         As it relates to the proposed transition of Across into a private company structure, this survey gauges your interest in two potential options: a token buy out or equity exchange. This survey signals your participation interest in exchange or sale; however, this non-biding survey is contingent on the proposal&#39;s vote and legal permissibility. Read the full temp-check proposal <Link className="text-aqua-100 underline" href="https://across.to/">HERE</Link>.
